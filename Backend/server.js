5dbaa3e2c9c9c7e95a43d8f0f21b75a7661b6f67
Harmeet <102729017+Harmeet03@users.noreply.github.com>
2024-07-10 21:14:44 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -88,9 +88,8 @@ async function sendOTP(email, otp){
     try{
         const transporter = nodemailer.createTransport({
           // service: 'Outlook',
-          host: 'smtp.office365.com',
+          host: 'smtp.ethereal.email',
           port: 587,
-          secure: false,
           auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
