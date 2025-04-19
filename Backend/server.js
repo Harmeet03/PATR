14f38bfc52d930023ffa4415c82cda135f7ab57c
Harmeet Singh <hsdhanjal2003@gmail.com>
2025-04-20 00:34:35 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,7 @@ const cors = require("cors");
 database();
 
 const connection = {
+    // origin: 'http://localhost:3000',
     origin: 'https://patr.netlify.app',
     credentials: true
 };
@@ -19,12 +20,14 @@ app.use(cors(connection));
 const handleSubmit = async (e) => {
   e.preventDefault();
   try{
+      // const request = await fetch(`http://localhost:4040`, {
       const request = await fetch('https://patr-202b.onrender.com', {
           method: "POST",
           mode: "cors",
           headers: {
               'Content-Type': 'application/json',
               'Accept': 'application/json',
+              // 'Access-Control-Allow-Origin': 'http://localhost:3000'
               'Access-Control-Allow-Origin': 'https://patr.netlify.app'
           }
       });
